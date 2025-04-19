8ae1293200ef518dc0b536690891b3f4dd182759
jx <1483010246@qq.com>
2025-04-19 23:13:47 +0800

    Initial commit

---
+++ ucontexttest.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<ucontext.h>
+#include<unistd.h>
+
+int main() {
+    ucontext_t context;
+    getcontext(&context);
+    printf("ucontext_t size: %zu\n", sizeof(context));
+    printf("ucontext_t uc_mcontext size: %zu\n", sizeof(context.uc_mcontext));
+    printf("ucontext_t uc_stack size: %zu\n", sizeof(context.uc_stack));
+    sleep(1);
+    setcontext(&context);
+    printf("ucontext_t uc_link size: %zu\n", sizeof(context.uc_link));
+    printf("ucontext_t uc_sigmask size: %zu\n", sizeof(context.uc_sigmask));
+    return 0;
+}
